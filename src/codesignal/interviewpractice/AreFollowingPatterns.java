844a620470a1a86e94f179521c373fa11f974b94
gong.hanjin <richardgong1988@gmail.com>
2022-05-12 22:16:37 +0900

    practice

--- src/codesignal/interviewpractice/AreFollowingPatterns.java
+++ src/codesignal/interviewpractice/AreFollowingPatterns.java
@@ -1,31 +1,31 @@
 package codesignal.interviewpractice;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 public class AreFollowingPatterns {
     public static void main(String[] args) {
         AreFollowingPatterns s = new AreFollowingPatterns();
-        System.out.println(s.solution(new String[]{"cat", "dog", "doggy"}, new String[]{"a", "b", "b"}));
+        System.out.println(s.solution(new String[]{"cat", "dog", "dog"}, new String[]{"a", "b", "c"}));
     }
 
-    boolean solution(String[] strings, String[] patterns) {
-        if (strings.length != patterns.length) {
-            return false;
-        }
 
-        LinkedHashMap<String, String> stringsCounts = new LinkedHashMap<>();
+    boolean solution(String[] strings, String[] patterns) {
+        LinkedHashMap<String, String> map1 = new LinkedHashMap<>();
+        LinkedHashMap<String, String> map2 = new LinkedHashMap<>();
         for (int i = 0; i < strings.length; i++) {
             String string = strings[i];
             String pattern = patterns[i];
-            stringsCounts.computeIfAbsent(pattern, k -> string);
-
-            if (!stringsCounts.get(pattern).equals(string)) {
+            if (map1.containsKey(string) && !map1.get(string).equals(pattern)) {
                 return false;
             }
+
+            if (map2.containsKey(pattern) && !map2.get(pattern).equals(string)) {
+                return false;
+            }
+            map1.put(string, pattern);
+            map2.put(pattern, string);
+
         }
-
-
         return true;
     }
 
