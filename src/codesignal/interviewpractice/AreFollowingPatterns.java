215e13653ba54a85c98ce1ac9fcf0b89c7a81531
gong.hanjin <asdf7890>
2022-05-12 21:09:40 +0900

    codesignal

--- src/codesignal/interviewpractice/AreFollowingPatterns.java
+++ src/codesignal/interviewpractice/AreFollowingPatterns.java
@@ -4,19 +4,23 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class AreFollowingPatterns {
+    public static void main(String[] args) {
+        AreFollowingPatterns s = new AreFollowingPatterns();
+        System.out.println(s.solution(new String[]{"cat", "dog", "doggy"}, new String[]{"a", "b", "b"}));
+    }
+
     boolean solution(String[] strings, String[] patterns) {
         if (strings.length != patterns.length) {
             return false;
         }
 
-        LinkedHashMap<String, Integer> stringsCounts = new LinkedHashMap<>();
-        LinkedHashMap<String, Integer> patternsCounts = new LinkedHashMap<>();
+        LinkedHashMap<String, String> stringsCounts = new LinkedHashMap<>();
         for (int i = 0; i < strings.length; i++) {
             String string = strings[i];
             String pattern = patterns[i];
-            stringsCounts.put(string, stringsCounts.getOrDefault(string, 0) + 1);
-            patternsCounts.put(pattern, patternsCounts.getOrDefault(pattern, 0) + 1);
-            if (!patternsCounts.get(pattern).equals(stringsCounts.get(string))) {
+            stringsCounts.computeIfAbsent(pattern, k -> string);
+
+            if (!stringsCounts.get(pattern).equals(string)) {
                 return false;
             }
         }
