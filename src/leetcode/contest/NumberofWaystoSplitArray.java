0cc0e72aed1ad4cd14a0aca5658f659dc271c84e
gong.hanjin <richardgong1988@gmail.com>
2022-05-16 20:29:48 +0900

    practice

---
+++ src/leetcode/contest/NumberofWaystoSplitArray.java
@@ -0,0 +1,29 @@
+package leetcode.contest;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class NumberofWaystoSplitArray {
+    public static void main(String[] args) {
+        NumberofWaystoSplitArray s = new NumberofWaystoSplitArray();
+        System.out.println(s.waysToSplitArray(new int[]{10, 4, -8, 7}));
+    }
+
+    public int waysToSplitArray(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+
+        int result = 0;
+
+        int lSum = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            lSum += nums[i];
+            int rSum = sum - lSum;
+            if (lSum >= rSum) {
+                result++;
+            }
+        }
+
+
+        return result;
+    }
+}
