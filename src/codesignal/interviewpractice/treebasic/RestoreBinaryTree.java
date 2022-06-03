5a42094cd1eee80299d4cc74de7f3c123c7559ee
gong.hanjin <richardgong1988@gmail.com>
2022-06-03 10:11:02 +0900

    practice

--- src/codesignal/interviewpractice/treebasic/RestoreBinaryTree.java
+++ src/codesignal/interviewpractice/treebasic/RestoreBinaryTree.java
@@ -2,35 +2,41 @@ package codesignal.interviewpractice.treebasic;
 
 import codesignal.interviewpractice.Tree;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class RestoreBinaryTree {
-    Tree<Integer> solution(int[] inorder, int[] preorder) {
-        int j = 0;
-        Deque<Tree<Integer>> stack = new LinkedList<>();
-        Tree<Integer> root = new Tree<>(preorder[0]);
-        stack.push(root);
-        Tree<Integer> node = root;
-        for (int i = 1; i < preorder.length; i++) {
-            boolean popped = false;
-            while (!stack.isEmpty() && stack.peek().value.equals(inorder[j])) {
-                j++;
-                node = stack.pop();
-                popped = true;
-            }
-            if (popped) {
-                Tree<Integer> next = new Tree<>(preorder[i]);
-                node.right = next;
-                stack.push(next);
-                continue;
-            }
-            Tree<Integer> next = new Tree<>(preorder[i]);
-            if (stack.peek() != null) {
-                stack.peek().left = next;
-            }
-            stack.push(next);
-        }
-        return root;
+
+    public static void main(String[] args) {
+        RestoreBinaryTree restoreBinaryTree = new RestoreBinaryTree();
+        restoreBinaryTree.solution(new int[]{4, 2, 1, 5, 3, 6}, new int[]{1, 2, 4, 3, 5, 6});
     }
+
+    Tree<Integer> solution(int[] inorder, int[] preorder) {
+        if (inorder.length == 0) {
+            return null;
+        }
+
+        var rootValue = preorder[0];
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootValue) {
+                var root = new Tree<>(rootValue);
+                root.left = solution(getArr(0, i - 1, inorder), getArr(1, i, preorder));
+                root.right = solution(getArr(i + 1, inorder.length - 1, inorder), getArr(i + 1, preorder.length - 1, preorder));
+                return root;
+            }
+        }
+
+        return null;
+    }
+
+    int[] getArr(int start, int end, int[] arr) {
+        var len = end - start + 1;
+        var result = new int[len];
+
+        int index = 0;
+        for (int i = start; i <= end; i++) {
+            result[index] = arr[i];
+            index++;
+        }
+        return result;
+    }
+
 }
---
+++ src/leetcode/LeetCode889.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class LeetCode889 {
+    public static void main(String[] args) {
+        LeetCode889 l = new LeetCode889();
+        TreeNode treeNode = l.constructFromPrePost(new int[]{1, 2, 4, 5, 3, 6, 7}, new int[]{4, 5, 2, 6, 7, 3, 1});
+        System.out.println(treeNode);
+    }
+
+    // 存储 postorder 中值到索引的映射
+    HashMap<Integer, Integer> valToIndex = new HashMap<>();
+
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        for (int i = 0; i < postorder.length; i++) {
+            valToIndex.put(postorder[i], i);
+        }
+        return build(preorder, 0, preorder.length - 1, 0, postorder.length - 1);
+    }
+
+    // 定义：根据 preorder[preStart..preEnd] 和 postorder[postStart..postEnd]
+    // 构建二叉树，并返回根节点。
+    TreeNode build(int[] preorder, int preStart, int preEnd, int postStart, int postEnd) {
+        if (preStart > preEnd) {
+            return null;
+        }
+        if (preStart == preEnd) {
+            return new TreeNode(preorder[preStart]);
+        }
+
+        // root 节点对应的值就是前序遍历数组的第一个元素
+        int rootVal = preorder[preStart];
+        // root.left 的值是前序遍历第二个元素
+        // 通过前序和后序遍历构造二叉树的关键在于通过左子树的根节点
+        // 确定 preorder 和 postorder 中左右子树的元素区间
+        int leftRootVal = preorder[preStart + 1];
+        // leftRootVal 在后序遍历数组中的索引
+        int index = valToIndex.get(leftRootVal);
+        // 左子树的元素个数
+        int leftSize = index - postStart + 1;
+
+        // 先构造出当前根节点
+        TreeNode root = new TreeNode(rootVal);
+
+
+        // 递归构造左右子树
+        // 根据左子树的根节点索引和元素个数推导左右子树的索引边界
+        root.left = build(preorder, preStart + 1, preStart + leftSize, postStart, index);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd, index + 1, postEnd - 1);
+
+        return root;
+    }
+}
