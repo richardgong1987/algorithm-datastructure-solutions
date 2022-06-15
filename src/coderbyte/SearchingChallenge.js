3a218379ee0c3335a06985ebfb9fe609bb9d7dba
gong.hanjin <richardgong1988@gmail.com>
2022-06-15 20:47:41 +0900

    practice

---
+++ src/coderbyte/SearchingChallenge.js
@@ -0,0 +1,37 @@
+function SearchingChallenge(strArr) {
+    let ret = [];
+    let indexs = [];
+    let subStr = [];
+    for (const element of strArr) {
+        subStr = element.split("");
+        let index = [];
+        for (let y = 0; y < subStr.length; y++) {
+            if (subStr[y] == 0) {
+                index.push(y);
+            }
+            if (y == subStr.length - 1) {
+                indexs.push(index);
+            }
+        }
+    }
+    for (let i = 0; i < indexs.length; i++) {
+        for (let j = 0; j < indexs[i].length; j++) {
+            if (indexs[i + 1] && (indexs[i][j] == indexs[i + 1][j] || indexs[i + 1].indexOf(indexs[i][j]))) {
+                if (ret.indexOf(strArr[i]) == -1) {
+                    ret.push(strArr[i]);
+                }
+            }
+
+            if (Math.abs(indexs[i][j] - indexs[i][j + 1]) == 1) {
+                if (ret.indexOf(strArr[i]) == -1) {
+                    ret.push(strArr[i])
+                }
+            }
+        }
+    }
+
+    return ret.length;
+}
+
+// keep this function call here
+console.log(SearchingChallenge(["1011", "0010"]));
---
+++ src/coderbyte/stockMaxProfix.js
@@ -0,0 +1,22 @@
+function ArrayChallenge(arr) {
+
+    var maxProfit = -1;
+    var buyPrice = 0;
+    var change = true;
+
+    for (var i = 0; i < arr.length - 1; i++) {
+        let sellPrice = arr[i + 1];
+        if (change) {
+            buyPrice = arr[i];
+        }
+        change = sellPrice < buyPrice;
+        if (change === false) {
+            maxProfit = Math.max(sellPrice - buyPrice, maxProfit);
+        }
+    }
+
+    return maxProfit;
+
+}
+
+console.log(ArrayChallenge([10, 12, 4, 5, 9]))
