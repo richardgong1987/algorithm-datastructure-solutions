2920757e8d86596fe22b5d37975707db515fdca8
gong.hanjin <richardgong1988@gmail.com>
2022-07-02 23:37:17 +0900

    rettailStore

---
+++ src/codility/beBit/RetailStore.java
@@ -0,0 +1,58 @@
+package codility.beBit;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * ![](./img.png)
+ *
+ */
+public class RetailStore {
+    public int solution(int k, int[][] A) {
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (A[i][j] == 1) {
+                    set.add(i + "#" + j);
+                }
+
+            }
+        }
+        int nWest = Integer.MIN_VALUE;
+        int sEast = Integer.MAX_VALUE;
+
+        int sWest = Integer.MIN_VALUE;
+        int nEast = Integer.MAX_VALUE;
+
+
+        for (String house : set) {
+            String[] hs = house.split("#");
+            int a = Integer.valueOf(hs[0]);
+            int b = Integer.valueOf(hs[1]);
+
+            nWest = Math.max(nWest, a - b - k);
+            sEast = Math.min(sEast, a - b + k);
+
+            sWest = Math.max(sWest, a + b - k);
+            nEast = Math.min(nEast, a + b + k);
+
+        }
+        int count = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (A[i][j] == 0) {
+                    if (nWest <= i - j && i - j <= sEast) {
+                        if (sWest <= i + j && i + j <= nEast) {
+                            count++;
+                        }
+                    }
+                }
+
+            }
+        }
+        return count;
+
+    }
+}
